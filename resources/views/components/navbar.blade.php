f07fc94521ebeaeb8fe4c6654d16269e43112d6b
Taiseer <77081440+TaiseerAlmedani@users.noreply.github.com>
2021-06-17 17:38:04 +0300

    create layouts

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,51 @@
+<nav style="margin-bottom: 100px;background-color:#37424c" class="navbar is-fixed-top" role="navigation"
+    aria-label="main navigation">
+    <div class="navbar-brand">
+        <a class="navbar-item" href="#">
+            <img src="https://w7.pngwing.com/pngs/826/933/png-transparent-learning-management-system-education-lms.png" width="100" height="40">
+        </a>
+
+        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+        </a>
+    </div>
+
+    <div id="navbar-example" class="navbar-menu">
+
+        <div class="navbar-start">
+
+            <a class="navbar-item is-tab" href="#" style="color: white">
+                Home
+            </a>
+
+            <a class="navbar-item is-tab" style="color: white">
+                About Us
+            </a>
+
+            <a class="navbar-item is-tab" style="color: white">
+                Contact Us
+            </a>
+
+        </div>
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <div class="buttons">
+
+                        <a class="button is-light" href="#">
+                            <strong>Log in</strong>
+                        </a>
+
+                        <a class="button " href="#">
+                            Register
+                        </a>
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+</nav>
