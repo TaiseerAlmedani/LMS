e2cb8f207223ad4dfbb65a00ec2b4c919cabb067
Essam Dalal <essamdalal7@gmail.com>
2021-06-17 20:46:58 +0300

    fix home page

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,51 +1,50 @@
-<nav style="margin-bottom: 100px;background-color:#37424c" class="navbar is-fixed-top" role="navigation"
-    aria-label="main navigation">
-    <div class="navbar-brand">
-        <a class="navbar-item" href="#">
-            <img src="https://w7.pngwing.com/pngs/826/933/png-transparent-learning-management-system-education-lms.png" width="100" height="40">
-        </a>
-
-        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-        </a>
-    </div>
-
-    <div id="navbar-example" class="navbar-menu">
-
-        <div class="navbar-start">
-
-            <a class="navbar-item is-tab" href="#" style="color: white">
-                Home
+<nav class="navbar">
+    <div class="container">
+        <div class="navbar-brand">
+            <a class="navbar-item" href="../">
+                <img src="http://bulma.io/images/bulma-type-white.png" alt="Logo">
             </a>
-
-            <a class="navbar-item is-tab" style="color: white">
-                About Us
-            </a>
-
-            <a class="navbar-item is-tab" style="color: white">
-                Contact Us
-            </a>
-
+            <span class="navbar-burger burger" data-target="navbarMenu">
+                <span></span>
+                <span></span>
+                <span></span>
+            </span>
         </div>
+        <div id="navbarMenu" class="navbar-menu">
             <div class="navbar-end">
-                <div class="navbar-item">
-                    <div class="buttons">
-
-                        <a class="button is-light" href="#">
-                            <strong>Log in</strong>
-                        </a>
-
-                        <a class="button " href="#">
-                            Register
-                        </a>
-
-
-                    </div>
-                </div>
+                <span class="navbar-item">
+                    <a class="button is-white is-outlined" href="#">
+                        <span class="icon">
+                            <i class="fa fa-home"></i>
+                        </span>
+                        <span>Home</span>
+                    </a>
+                </span>
+                <span class="navbar-item">
+                    <a class="button is-white is-outlined" href="#">
+                        <span class="icon">
+                            <i class="fa fa-superpowers"></i>
+                        </span>
+                        <span>Examples</span>
+                    </a>
+                </span>
+                <span class="navbar-item">
+                    <a class="button is-white is-outlined" href="#">
+                        <span class="icon">
+                            <i class="fa fa-book"></i>
+                        </span>
+                        <span>Documentation</span>
+                    </a>
+                </span>
+                <span class="navbar-item">
+                    <a class="button is-white is-outlined" href="https://github.com/BulmaTemplates/bulma-templates/blob/master/templates/landing.html">
+                        <span class="icon">
+                            <i class="fa fa-github"></i>
+                        </span>
+                        <span>View Source</span>
+                    </a>
+                </span>
             </div>
         </div>
-
-
+    </div>
 </nav>
